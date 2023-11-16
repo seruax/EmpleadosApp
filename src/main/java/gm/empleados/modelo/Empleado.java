beaff1054033db386859f5385a076a275a5c6a7d
SERGI-PC\sigma <seruaxj@gmail.com>
2023-11-16 13:21:14 +0100

    App upload

---
+++ src/main/java/gm/empleados/modelo/Empleado.java
@@ -0,0 +1,25 @@
+package gm.empleados.modelo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado; // En bb_dd se crea como id_empleado
+    String nombreEmpleado;
+    String departamento;
+    String sueldo;
+}
---
+++ src/main/webapp/WEB-INF/jsp/comunes/cabecero.jsp
@@ -0,0 +1,11 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <title>Sistema de Empleados</title>
+</head>
+<body>
